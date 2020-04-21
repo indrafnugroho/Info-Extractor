b5ad8f88dd5c5e343bf8c5af04e16d88242a31de
Indra Nugroho <indrafbrngrh@gmail.com>
2020-04-22 01:48:34 +0700

    i have done my best

--- src/App.py
+++ src/App.py
@@ -1,18 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template(
-    'app.html')
+@app.route("/", methods=["POST", "GET"])
+def homePage():
+	if request.method == "POST":
+		files = request.files.getlist("file")
+		# print(files)
+		# return ""
+		return redirect(url_for("keyPage", fls=files))
+	else:
+		return render_template('app.html')
 
-# @app.route("/upload", methods=["POST"])
-# def upload():
-#     uploaded_files = flask.request.files.getlist("file[]")
-#     print (uploaded_files)
-#     return render_template(
-#     'app.html')
+@app.route("/keyword", methods=["POST", "GET"])
+def keyPage(fls):
+	if request.method == "POST":
+		keyword = request.form["kw"]
+		return redirect(url_for("algPage", fls=fls, kw=keyword))
+	else:
+		return render_template('keyword.html')
+
+@app.route("/algorithm", methods=["POST", "GET"])
+def algPage(fls, kw):
+	if request.method == "POST":
+		algorithm = "Boyer Moore"
+		return redirect(url_for("resPage", fls=fls, kw=kw, alg=algorithm))
+	else:
+		return render_template('algorithm.html')
+
+@app.route("/result")
+def resPage(fls, kw, alg):
+	return f"<h1>{kw}</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- src/Extractor.py
+++ src/Extractor.py
@@ -3,28 +3,52 @@
 # Regex
 
 import re
+import os
 from nltk import sent_tokenize
 from BM import buildLastOccurence, BMMatch
 from KMP import KMPMatch, calculateLPS
 
-file = open('../test/1.txt', 'r')
-text = file.read()
-arrOfWords = sent_tokenize(text)
+def algChooser(alg, text, keyword):
+	idx = -1
+	if (alg == "Knuth-Morris-Pratt"):
+		idx = KMPMatch(text.lower(), keyword.lower())
+	elif (alg == 'Boyer Moore'):
+		idx = BMMatch(text.lower(), keyword.lower())
+	else:
+		idx = RegexMatch(text.lower(), keyword.lower())
+	return idx
 
-for words in arrOfWords:
-	if (KMPMatch(words.lower(), 'positif') > 0):
-		print(words)
-		print("Check")
-		reTotal = re.compile(r'(\d{1,3}(\.|\,))*\d{1,} (([Oo]rang)|([Pp]asien)|([OoPp]dp)|([Kk]asus))')
-		totalRes = reTotal.search(words)
-		if (totalRes):
-			print("Total: ", end="")
-			print(totalRes.group())
+def RegexMatch(text, pattern):
+	if (re.search(pattern, text)):
+		return 1
+	else:
+		return 0
 
-		reTime = re.compile(r'([A-Za-z]{4,6}\,? )?\(?\d{1,2}[\/\- ](\d{1,2}|[a-zA-Z]{3,})[\/\- ]?\d{2,4}\)?( (pukul )?\d{1,2}[\.\:]\d{1,2} ((WITA)|(WI[BT])))?')
-		timeRes = reTime.search(words)
-		if (timeRes):
-			print("Time: ", end="")
-			print(timeRes.group())
+path = "../test/"
+files = os.listdir(path)
+for f in files:
+	if (f.endswith(".txt")):
+		file = open(path + f, 'r')
+		text = file.read()
+		arrOfWords = sent_tokenize(text)
 
-		print("")
+		for words in arrOfWords:
+			if (KMPMatch(words.lower(), 'positif') > 0):
+				print("Keyword(s): ", end="")
+				print('positif')
+				reTotal = re.compile(r'(\d{1,3}(\.|\,))*\d{1,} (([Oo]rang)|([Pp]asien)|([OoPp]dp)|([Kk]asus))')
+				totalRes = reTotal.search(words)
+				if (totalRes):
+					print("Total: ", end="")
+					print(totalRes.group())
+
+				reTime = re.compile(r'([A-Za-z]{4,6}\,? )?\(?\d{1,2}[\/\- ](\d{1,2}|[a-zA-Z]{3,})[\/\- ]?\d{2,4}\)?( (pukul )?\d{1,2}[\.\:]\d{1,2} ((WITA)|(WI[BT])))?')
+				timeRes = reTime.search(words)
+				if (timeRes):
+					print("Time: ", end="")
+					print(timeRes.group())
+
+				print(words, end=" ")
+				print("(", f, ")")
+
+				print("")
